a424286ead237f31fcbee1003aff178f8d2f1c7b
thomashoover <tuh92@psu.edu>
2018-11-27 22:09:48 -0500

    Added Different Inventory Systems

---
+++ src/edu/psu/cmpsc221/model/Item.java
@@ -0,0 +1,29 @@
+package edu.psu.cmpsc221.model;
+
+public class Item {
+    public Item(String name, String lookDescription, int weight, int bulkiness) {
+        this.lookDescription = lookDescription;
+        this.name = name;
+    } /* end Item */
+
+    public String getLookDescription() {
+        return lookDescription;
+    } /* end getLookDescription */
+
+    public String getName() {
+        return name;
+    } /* end getName */
+
+    public int getWeight(){
+        return weight;
+    }/*end getWeight*/
+
+    public int getBulkiness(){
+        return bulkiness;
+    }/*end getBulkiness*/
+
+    private String lookDescription;
+    private String name;
+    private int weight;
+    private int bulkiness;
+} /* end Item */
